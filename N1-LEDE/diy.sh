c897ef2aa7d8158fd3260b6cb79c82db1174dfa7
ADKill Player Offline <yndoc@users.noreply.github.com>
2025-10-04 18:47:03 +0800

    Update diy.sh
    修改登录管理IP

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -37,7 +37,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/luci/applications/luci-app-design-config
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.254/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
